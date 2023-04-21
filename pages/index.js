09c954b70f74dd06c478e60e7d6809c48d55f389
habsfanhak <56047585+habsfanhak@users.noreply.github.com>
2023-04-21 13:00:02 -0400

    changed title card

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,9 @@ export default function Home() {
     <>
       <Card>
           <Card.Img src="https://upload.wikimedia.org/wikipedia/commons/3/30/Metropolitan_Museum_of_Art_%28The_Met%29_-_Central_Park%2C_NYC.jpg"/><br/>
-          <Card.Text>This is the Metro Museum of Art, the most visited art museum in the western hemisphere. I don&apos;t know
-            why I have to show this specifically, but you can read more about it <a href="https://en.wikipedia.org/wiki/Metropolitan_Museum_of_Art" target="_blank" rel="noreferrer">here.</a>
+          <Card.Text>This is the Metro Museum of Art, the most visited art museum in the western hemisphere. This application utilizes a back-end API that handles both calls to this 
+            museum&apos;s API and actions with the user database, using the express framework. The database is hosted by MongoDB Atlas. The front-end work is all done with React JS. This 
+            project was completed on April 8, 2023, and you can learn more about the museum <a href="https://en.wikipedia.org/wiki/Metropolitan_Museum_of_Art" target="_blank" rel="noreferrer">here.</a>
 
           </Card.Text>
           <br/>
